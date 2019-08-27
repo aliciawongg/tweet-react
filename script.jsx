9f27a1c615e461404bc2d27296c5a8b800b0d0a9
aliciawongg <w.alicia@gmail.com>
2019-08-28 01:12:08 +0800

    added components to render list of tweets

--- script.jsx
+++ script.jsx
@@ -1,7 +1,45 @@
-var monkey = "hello";
+
+class TweetSingle extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                <img src={this.props.profileimg}/> {this.props.name} @{this.props.screenname}<br/>
+                {this.props.date}<br/>
+                {this.props.tweet}<br/>
+                Retweets: {this.props.retweetnum} Likes: {this.props.likenum}
+                </li>
+            </ul>
+        );
+    }
+}
+
+class TweetList extends React.Component {
+    render() {
+        let tweetThings = tweets.map(tweet => {
+            return <TweetSingle
+            profileimg={tweet.user.profile_image_url}
+            name={tweet.user.name}
+            screenname={tweet.user.screen_name}
+            date={tweet.created_at}
+            tweet={tweet.text}
+            retweetnum={tweet.retweet_count}
+            likenum={tweet.favorite_count}
+            >
+            </TweetSingle>
+        });
+        console.log('showing');
+        console.log(tweetThings);
+            return (
+                <ul>
+                    {tweetThings}
+                </ul>
+            );
+        }
+}
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <TweetList></TweetList>,
     document.getElementById('root')
 );
